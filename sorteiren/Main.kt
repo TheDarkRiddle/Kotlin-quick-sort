ffe5bd754a2bb43ef77e4aab35ad03a8e89b58c1
TheDarkRiddle <85240633+TheDarkRiddle@users.noreply.github.com>
2021-06-02 21:40:07 +0200

    Updated the main, to use quickSortPeople

--- sorteiren/Main.kt
+++ sorteiren/Main.kt
@@ -1,30 +1,19 @@
 package sortieren
+import sorteiren.*
 import java.util.Comparator
-import  sorteiren.People
-import sorteiren.English
-import sorteiren.German
-            //MAIN
-fun main() {
-                val intComperator = Comparator<People>{ o1: People, o2:People -> when{
 
-                    o1.alter == o2.alter -> 0
-                    o1.alter < o2.alter -> -1
-                    o1.alter > o2.alter -> 1
-                    else -> 0
-                }
-                }
-    val Crowd=Liste<People>();
+//MAIN
+fun main() {              
+    val Crowd = Liste<People>();
 
-    var test=German("Guenter",1)
-    Crowd.addfirst(test)
+    Crowd.addfirst(German("Guenter",10))
     Crowd.addfirst(English("Mark",2))
     Crowd.addfirst(German("Haarald",3))
     Crowd.addlast(English("Kai",4))
-
+    
     Crowd.forEach { println("My name is: ${it.name}(Age: ${it.alter})") };
     println("SORTED:")
-    //   Crowd.quickSort(intComperator);
-    println()
+    Crowd.replace(quickSortPeople(Crowd))
     Crowd.forEach { println("My name is: ${it.name}(Age: ${it.alter})") };
 
-            }
+    }
